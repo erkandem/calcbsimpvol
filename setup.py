44563b95da395b3ce56951db6e83bddb0d78471d
kan <erkan.dem@pm.me>
2020-04-09 14:53:27 +0200

    update email address in `setup.py`

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     long_description_content_type='text/markdown',
     url='https://erkandem.github.io/calcbsimpvol/',
     author='Erkan Demiralay',
-    author_email='erkan@erkan.io',
+    author_email='erkan.dem@pm.me',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
