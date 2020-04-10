5b342ccecd9c4737d887aa2c25e96604639cbf54
kan <erkan.dem@pm.me>
2020-04-10 14:48:56 +0200

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='calcbsimpvol',
-    version='1.13.0',
+    version='1.14.0',
     license='MIT',
     description='Calculate Black Scholes Implied Volatility - Vectorwise ',
     long_description=long_description,
